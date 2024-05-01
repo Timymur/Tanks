ffd73618820625b36b2d3dbfb34ba335f4a90f40
unknown <hitechmag2013@mail.ru>
2024-05-01 11:03:57 +0500

    move_update

--- src/tank.js
+++ src/tank.js
@@ -13,12 +13,40 @@ export default class Tanks{
         [28*4, 3, 25, 25,], // down
         [28*5, 0, 25, 25,],
 
-        [28*2, 0, 25, 25,], //
+        [28*2, 0, 25, 25,], // left
         [28*3, 0, 25, 25,]
     ];
 
     get sprite(){ // возвращает нужный спрайт в зависмости от direction
         return this.frames[this.direction*2 + this.animationFrame];
+    }
+
+    update(activeKeys){
+        if(activeKeys.has("ArrowUp")){
+            this.move(0, 'y', -1);
+        }
+        else 
+        if(activeKeys.has("ArrowDown")){
+            this.move(2, 'y', 1);
+        
+        }
+        else 
+        if(activeKeys.has("ArrowRight")){
+            this.move(1, 'x', 1);
+            
+        }
+        else 
+        if(activeKeys.has("ArrowLeft")){
+            this.move(3, 'x', -1);
+            
+       }
+    }
+
+    move(direction, axis, value){
+        this.direction = direction; // меняем направление
+        this[axis]+=value; // меняем позицию танка
+        this.animationFrame ^= 1; // ^ булевый оператор используется для переключения на противоположное. Toggle. T - триггер.
+
     }
 
 
--- src/world.js
+++ src/world.js
@@ -8,31 +8,7 @@ export default class World{ // Мир
 
     update(activeKeys){
        
-        if(activeKeys.has("ArrowUp")){
-            this.player1Tank.y-=1; // меняем позицию танка
-            this.player1Tank.direction = 0; // меняем направление
-            this.player1Tank.animationFrame ^= 1; // ^ булевый оператор используется для переключения на противоположное. Toggle. T - триггер.
-
-        }
-        else 
-        if(activeKeys.has("ArrowDown")){
-            this.player1Tank.y+=1;
-            this.player1Tank.direction = 2;
-            this.player1Tank.animationFrame ^= 1;
-        }
-        else 
-        if(activeKeys.has("ArrowRight")){
-            this.player1Tank.x+=1;
-            this.player1Tank.direction = 1;
-            this.player1Tank.animationFrame ^= 1;
-        }
-        else 
-        if(activeKeys.has("ArrowLeft")){
-            this.player1Tank.x-=1;
-            this.player1Tank.direction = 3;
-            this.player1Tank.animationFrame ^= 1;
-            
-       }
+        this.player1Tank.update(activeKeys);
 
     }
 }
